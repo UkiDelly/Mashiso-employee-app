3d3ea2f212ffd3e5dbf6bc3edc3d2221d15d76b6
Dae Hyeon <daehyeon1999@naver.com>
2022-05-24 12:22:36 +0800

    Start

---
+++ android/app/src/main/kotlin/com/mashiso/employee_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mashiso.employee_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/record.dart
@@ -0,0 +1,5 @@
+class Record {
+  final String userId;
+  final DateTime time;
+  final GeoPoint location;
+}
